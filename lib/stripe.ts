c6ca5fdf6cd673430fcbdee269301cf89dce9f72
muhammadmaaz2k5 <22pwbcs0933@uetpeshawar.edu.pk>
2026-07-20 19:05:02 +0500

    added stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 // CHANGE THIS LINE TO GO LIVE: replace the test key with your live STRIPE_SECRET_KEY in .env.local
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_mock_key', {
   apiVersion: '2026-03-25.dahlia',
 });
 
